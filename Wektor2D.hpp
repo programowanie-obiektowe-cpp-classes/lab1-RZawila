8581b78958c50d7f65f1108b61a3f13feee4ad52
RZawila <ryszaw46@gmail.com>
2022-10-23 19:57:24 +0200

    Wersja 2 do sprawdzenia

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -2,25 +2,26 @@
 class Wektor2D
 {
     public:
-    //konstruktor domuślny
-    Wektor2D(){x=0;y=0;}
-   //konstruktor parametryczny
-   Wektor2D(double u,double v){
-   x=u;
-   y=v;
-   }
-    //destruktor
+//konstruktor domuślny
+    Wektor2D(){x=0.;y=0.;}
+//konstruktor parametryczny
+    Wektor2D(double u,double v)
+    {
+    x=u;
+    y=v;
+    }
+//destruktor
     /*
      ~Wektor2D()
     {
         std::cout<<"I was nice beeing your vector"<<"\n";
     }
     */
-    //settery
+//settery
     void setX(double a){x=a;}
     void setY(double b){y=b;}
 
-    //getter
+//getter
     double getX(){return x;}
     double getY(){return y;}
 
@@ -34,35 +35,25 @@ class Wektor2D
         std::cout<<"Wspolrzedna X:"<<x<<"\t Wspolrzedna y:"<<y<<"\n";
     }
 
-    //overload +
+//overload +
     Wektor2D operator + (Wektor2D &vec)
     {
         //suma wekrorów to nowy wektor?
-         Wektor2D res;
-         res.setX(x+vec.getX());
-         res.setY(y+vec.getY());
-         return res;
+         Wektor2D v;
+         v.setX(x+vec.getX());
+         v.setY(y+vec.getY());
+         return v;
 
     }
-
 //overload *
     double operator * (Wektor2D &v1)
     {
-
-
         double product = x*v1.getX()+y*v1.getY();
 
         return product;
     }
-
-
-
-
-
-
     private:
-
     double x;
-    double y; // prywatne zmienne wspó³rzêdne wektora x i y
+    double y; // prywatne zmienne wspólrzêdne wektora x i y
 };
 
